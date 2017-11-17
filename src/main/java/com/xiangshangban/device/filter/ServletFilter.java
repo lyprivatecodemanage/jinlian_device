27ae5a6e546659cc073f91a8415ce70a2de3a412
liuguanglong <liuguanglong@xiangshangban.com>
2017-11-17 15:49:19 +0800

    过滤器

--- src/main/java/com/xiangshangban/device/filter/ServletFilter.java
+++ src/main/java/com/xiangshangban/device/filter/ServletFilter.java
@@ -1,18 +1,12 @@
 package com.xiangshangban.device.filter;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.URLDecoder;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /*import com.xiangshangban.transit_service.util.HttpClientUtil;*/
 
@@ -20,17 +14,17 @@ import javax.servlet.http.HttpServletResponse;
 public class ServletFilter implements Filter{
 
 	public static void uploadFile(byte[] file, String filePath, String fileName) throws Exception {
-        File targetFile = new File(filePath);  
-        if(!targetFile.exists()){    
-            targetFile.mkdirs();    
-        }       
+        File targetFile = new File(filePath);
+        if(!targetFile.exists()){
+            targetFile.mkdirs();
+        }
         FileOutputStream out = new FileOutputStream(filePath+fileName);
         out.write(file);
         out.flush();
         out.close();
     }
-	
-	
+
+
 	public void init(FilterConfig filterConfig) throws ServletException {
 		System.out.println("进来了");
 	}
@@ -64,7 +58,7 @@ public class ServletFilter implements Filter{
 				redirect = true;
 			}
 		}
-		
+
 		if(redirect){
 			//System.out.println("sessionId :"+req.getSession().getId());
 			//String companyId = req.getParameter("companyId");
